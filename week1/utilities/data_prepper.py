a1c436f113fc9e6c2c816aa5058b1f1fa713544c
pdavalo <davalo@gmail.com>
2022-06-25 08:33:37 +0000

    level 1

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -232,7 +232,8 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+        response = self.opensearch.search(body=log_query, index=self.index_name)
+        features_list = response["hits"]["hits"][0]["fields"]["_ltrlog"][0]["log_entry"]
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
         feature_results = {}
@@ -240,12 +241,15 @@ class DataPrepper:
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
+        for feature in features_list:
+            feature_results[feature["name"]] = []
         for doc_id in query_doc_ids:
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            for feature in features_list:
+                print(feature)
+                feature_results[feature["name"]].append(feature.get("value", 0))
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
